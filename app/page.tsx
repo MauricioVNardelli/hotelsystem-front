e8875f0a17cbafed20624097e3eb3c39d983ea1e
mauricio nardelli <mauricio.nardelli@hotmail.com>
2023-11-21 20:50:08 -0300

    Commit

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link';
 export default function Home() {
   return (
     <div>
-      <h1>Hotel System</h1>
+      <h1>Hotel System2</h1>
 
       <Link href="/login">
         <span>Entrar</span>
