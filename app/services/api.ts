a49ee23991c241be5136f4a96364b6ad3d832982
mauricio nardelli <mauricio.nardelli@hotmail.com>
2023-11-21 21:28:54 -0300

    commit

--- app/services/api.ts
+++ app/services/api.ts
@@ -5,6 +5,6 @@ import nookies from 'nookies';
 export const api = axios.create({
     baseURL: 'http://localhost:3333',
     headers: {
-      Authorization: `Bearer ${ "teste" }`
+      Authorization: `Bearer ${ "teste 2" }`
     }
   })
