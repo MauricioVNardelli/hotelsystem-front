38209a280baeb9277e19e337346f9898b4523f1f
Mauricio Nardelli <mauricio.vnardelli@gmail.com>
2023-11-23 07:44:35 -0300

    Commit

--- app/services/api.ts
+++ app/services/api.ts
@@ -3,7 +3,8 @@ import { parseCookies } from 'nookies';
 import nookies from 'nookies';
 
 export const api = axios.create({
-    baseURL: 'http://localhost:3333',
+    //baseURL: 'http://localhost:3333',
+    baseURL: 'https://back-end-hotel.onrender.com',
     headers: {
       Authorization: `Bearer ${ "teste 2" }`
     }
